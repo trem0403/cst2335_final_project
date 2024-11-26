288e2b6b79bb8b5969dc1f1b43148aaf7d854a86
trem0403 <156836799+trem0403@users.noreply.github.com>
2024-11-25 19:46:45 -0500

    Stylized header

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -22,7 +22,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Home Page',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: MyHomePage(
@@ -51,8 +50,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: const Color(0xFF14213D),
+        leading: const Icon(
+          Icons.home,  // Home icon
+          color: Colors.white,  // Icon color
+        ),
+        title: Text(widget.title, style: const TextStyle(color: Colors.white),),
       ),
       body: Center(
         child: Column(
