657094fa18846a1f784e56e08d22383f4fb9193e
trem0403 <156836799+trem0403@users.noreply.github.com>
2024-11-26 20:20:06 -0500

    Stylized main page

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Home Page',
       theme: ThemeData(
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF14213D)),
       ),
       home: MyHomePage(
         title: 'Home Page',
@@ -34,17 +35,37 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   final AppDatabase database;
+  final String title;
 
   const MyHomePage({super.key, required this.database, required this.title});
-
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  final List<Map<String, dynamic>> menuItems = [
+    {
+      'title': 'Customer List',
+      'icon': Icons.person,
+      'page': (context, database) => CustomerListPage(database: database),
+    },
+    {
+      'title': 'Car List',
+      'icon': Icons.directions_car,
+      'page': (context, database) => CarListPage(database: database),
+    },
+    {
+      'title': 'Car Dealership List',
+      'icon': Icons.storefront,
+      'page': (context, database) => CarDealershipListPage(database: database),
+    },
+    {
+      'title': 'Sales List',
+      'icon': Icons.attach_money,
+      'page': (context, database) => SalesListPage(database: database),
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -52,60 +73,94 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: const Color(0xFF14213D),
         leading: const Icon(
-          Icons.home,  // Home icon
-          color: Colors.white,  // Icon color
+          Icons.home,
+          color: Color(0xFFFCA311),
         ),
-        title: Text(widget.title, style: const TextStyle(color: Colors.white),),
+        title: Text(
+          widget.title,
+          style: const TextStyle(color: Colors.white),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CustomerListPage(database: widget.database),
+      body: Stack(
+        children: [
+          buildPositionedCard(
+            context,
+            menuItems[0],
+            Alignment.topLeft,
+          ),
+          buildPositionedCard(
+            context,
+            menuItems[1],
+            Alignment.topRight,
+          ),
+          buildPositionedCard(
+            context,
+            menuItems[2],
+            Alignment.bottomLeft,
+          ),
+          buildPositionedCard(
+            context,
+            menuItems[3],
+            Alignment.bottomRight,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildPositionedCard(
+      BuildContext context, Map<String, dynamic> item, Alignment alignment) {
+    // Get the media query orientation
+    var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
+    // Adjust width and height based on orientation
+    double cardWidth = isPortrait
+        ? MediaQuery.of(context).size.width * 0.45  // 45% of the screen width in portrait
+        : MediaQuery.of(context).size.width * 0.48; // 48% of the screen width in landscape
+    double cardHeight = isPortrait
+        ? MediaQuery.of(context).size.height * 0.43  // 43% of the screen height in portrait
+        : MediaQuery.of(context).size.height * 0.35; // 35% of the screen height in landscape
+
+    return Align(
+      alignment: alignment,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SizedBox(
+          width: cardWidth,
+          height: cardHeight,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => item['page'](context, widget.database),
+                ),
+              );
+            },
+            child: Card(
+              color: Color(0xFFFCA311),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              elevation: 5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(item['icon'], size: 48, color: Colors.white),
+                  const SizedBox(height: 12),
+                  Text(
+                    item['title'],
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
-                );
-              },
-              child: const Text('Customer List'),
+                ],
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CarListPage(database: widget.database),
-                  ),
-                );
-              },
-              child: const Text('Car List'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CarDealershipListPage(database: widget.database),
-                  ),
-                );
-              },
-              child: const Text('Car Dealership List'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SalesListPage(database: widget.database),
-                  ),
-                );
-              },
-              child: const Text('Sales List'),
-            ),
-          ],
+          ),
         ),
       ),
     );
